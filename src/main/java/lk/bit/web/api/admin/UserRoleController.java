5c89f256b9c1dd89eb033c232e8c0d05e449fd83
LawrenceAsanka <asankalorance@gmail.com>
2020-11-02 21:21:04 +0530

    Add new DTO - UserRoleDTO

--- src/main/java/lk/bit/web/api/admin/UserRoleController.java
+++ src/main/java/lk/bit/web/api/admin/UserRoleController.java
@@ -1,4 +1,15 @@
 package lk.bit.web.api.admin;
 
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+
+@CrossOrigin
+@RestController
+@RequestMapping("api/v1/roles")
 public class UserRoleController {
+
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
+    public static void addUserRole(@RequestBody ){
+
+    }
 }
---
+++ src/main/java/lk/bit/web/dto/UserRoleDTO.java
@@ -0,0 +1,14 @@
+package lk.bit.web.dto;
+
+import lombok.*;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Setter
+@Getter
+@ToString
+public class UserRoleDTO {
+
+    private String id;
+    private String name;
+}
