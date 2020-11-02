ea38abf81869503a85047e52a272e698d1cf85e9
LawrenceAsanka <asankalorance@gmail.com>
2020-11-02 18:10:03 +0530

    Test the SpringBoot

---
+++ src/main/java/lk/bit/web/api/admin/UserRegistrationController.java
@@ -0,0 +1,17 @@
+package lk.bit.web.api.admin;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@CrossOrigin
+@RestController
+@RequestMapping("api/v1/users")
+public class UserRegistrationController {
+
+    @GetMapping
+    public static String test(){
+        return "Happy coding my boy";
+    }
+}
