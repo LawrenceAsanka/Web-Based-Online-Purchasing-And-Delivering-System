4845bec57a41204682085f59aa13a2648905e6db
LawrenceAsanka <asankalorance@gmail.com>
2020-11-02 16:49:04 +0530

    Initial Commit

---
+++ src/main/java/lk/bit/web/AppInitializer.java
@@ -0,0 +1,14 @@
+package lk.bit.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@EnableWebMvc
+@SpringBootApplication
+public class AppInitializer {
+    public static void main(String[] args) {
+
+        SpringApplication.run(AppInitializer.class, args);
+    }
+}
