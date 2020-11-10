84c406f80943dd79feda92c042a17cbb6ef2f392
LawrenceAsanka <asankalorance@gmail.com>
2020-11-10 12:58:55 +0530

    Update UserRespository

--- src/main/java/lk/bit/web/repository/admin/UserRepository.java
+++ src/main/java/lk/bit/web/repository/admin/UserRepository.java
@@ -4,4 +4,6 @@ import lk.bit.web.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, String> {
+
+    User findFirstLastCustomerIdByOrderByIdDesc();
 }
--- src/main/java/lk/bit/web/repository/admin/UserRoleRepository.java
+++ src/main/java/lk/bit/web/repository/admin/UserRoleRepository.java
@@ -3,5 +3,5 @@ package lk.bit.web.repository.admin;
 import lk.bit.web.entity.UserRole;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserRoleRepository extends JpaRepository<UserRole,String> {
+public interface UserRoleRepository extends JpaRepository<UserRole, String> {
 }
