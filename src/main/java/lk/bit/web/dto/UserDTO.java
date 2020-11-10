33b00ef859bb2fa4695ba66b43d4fd3a85b27738
LawrenceAsanka <asankalorance@gmail.com>
2020-11-10 11:05:35 +0530

    Add new DTO - UserDTO

---
+++ src/main/java/lk/bit/web/dto/UserDTO.java
@@ -0,0 +1,24 @@
+package lk.bit.web.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Setter
+@Getter
+public class UserDTO {
+
+    private String id;
+    private String firstName;
+    private String lastName;
+    private String nic;
+    private String contactOne;
+    private String contactTwo;
+    private String username;
+    private String password;
+    private String userRole;
+    private String status;
+}
--- src/main/java/lk/bit/web/repository/admin/UserRepository.java
+++ src/main/java/lk/bit/web/repository/admin/UserRepository.java
@@ -1,5 +1,6 @@
 package lk.bit.web.repository.admin;
 
+import lk.bit.web.dto.UserDTO;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<UserDTO, String> {
