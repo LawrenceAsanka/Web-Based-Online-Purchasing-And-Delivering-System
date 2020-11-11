88bb821b5fd2f959f19fb807bb5ea2719725eb44
LawrenceAsanka <asankalorance@gmail.com>
2020-11-11 07:34:18 +0530

    Update ProductCategoryBoImpl

--- src/main/java/lk/bit/web/business/custom/impl/ProductCategoryBoImpl.java
+++ src/main/java/lk/bit/web/business/custom/impl/ProductCategoryBoImpl.java
@@ -2,32 +2,48 @@ package lk.bit.web.business.custom.impl;
 
 import lk.bit.web.business.custom.ProductCategoryBO;
 import lk.bit.web.dto.ProductCategoryDTO;
+import lk.bit.web.entity.ProductCategory;
+import lk.bit.web.repository.product.CategoryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class ProductCategoryBoImpl implements ProductCategoryBO {
+
+    @Autowired
+    private CategoryRepository categoryRepository;
+
     @Override
     public List<ProductCategoryDTO> getAllCategories() {
-        return null;
+        List<ProductCategory> allCategories = categoryRepository.findAll();
+        List<ProductCategoryDTO> categories = new ArrayList<>();
+        for (ProductCategory category : allCategories) {
+            categories.add(new ProductCategoryDTO(category.getCategoryId(), category.getCategoryName(),
+                    category.getStatus()));
+        }
+        return categories;
     }
 
     @Override
     public void saveCategory(ProductCategoryDTO category) {
-
+        categoryRepository.save(new ProductCategory(category.getCategoryId(), category.getCategoryName(),
+                category.getStatus()));
     }
 
     @Override
     public void updateCategory(ProductCategoryDTO category, String categoryId) {
-
+        categoryRepository.save(new ProductCategory(categoryId, category.getCategoryName(),
+                category.getStatus()));
     }
 
     @Override
     public void deleteCategory(String categoryId) {
-
+        categoryRepository.deleteById(categoryId);
     }
 
     @Override
     public boolean existCategory(String categoryId) {
-        return false;
+        return categoryRepository.existsById(categoryId);
     }
 }
--- src/main/java/lk/bit/web/repository/product/CategoryRepository.java
+++ src/main/java/lk/bit/web/repository/product/CategoryRepository.java
@@ -1,6 +1,7 @@
 package lk.bit.web.repository.product;
 
+import lk.bit.web.entity.ProductCategory;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface CategoryRepository extends JpaRepository<CategoryRepository, String> {
+public interface CategoryRepository extends JpaRepository<ProductCategory, String> {
 }
