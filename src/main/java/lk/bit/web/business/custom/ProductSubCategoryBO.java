34fb7c2ada742d3bb4f95eceb7c9302696e89474
LawrenceAsanka <asankalorance@gmail.com>
2020-11-11 09:09:34 +0530

    Create ProductSubCategoryBO

---
+++ src/main/java/lk/bit/web/business/custom/ProductSubCategoryBO.java
@@ -0,0 +1,17 @@
+package lk.bit.web.business.custom;
+
+import lk.bit.web.business.SuperBO;
+import lk.bit.web.dto.product_dto.SubCategoryDTO;
+
+import java.util.List;
+
+public interface ProductSubCategoryBO extends SuperBO {
+
+    List<SubCategoryDTO> getAllSubCategories();
+
+    void saveSubCategory(SubCategoryDTO subCategory);
+
+    void updateSubCategory(String name,String status,String categoryId,String subCategoryId);
+
+    void deleteSubCategory(String subCategoryId);
+}
