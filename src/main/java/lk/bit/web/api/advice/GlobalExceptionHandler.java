3d2bc4b842a14fc84a321b5caee16ef4da944d6a
LawrenceAsanka <asankalorance@gmail.com>
2020-11-26 20:06:35 +0530

    Add two global exceptions

--- src/main/java/lk/bit/web/api/advice/GlobalExceptionHandler.java
+++ src/main/java/lk/bit/web/api/advice/GlobalExceptionHandler.java
@@ -1,4 +1,24 @@
 package lk.bit.web.api.advice;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.server.ResponseStatusException;
+
+@RestControllerAdvice
 public class GlobalExceptionHandler {
+
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    @ExceptionHandler(Exception.class)
+    public String globalExceptionHandlerMethod(Exception e){
+        e.printStackTrace();
+        return "Call DEPPO immediately";
+    }
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(ResponseStatusException.class)
+    public String globalResponseStatusExceptionHandlerMethod(){
+        return "Do not insert wrong values!";
+    }
 }
