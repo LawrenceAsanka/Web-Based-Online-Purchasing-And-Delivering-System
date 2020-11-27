93ee3d23c2bde63506f076145113919a4671a7bf
LawrenceAsanka <asankalorance@gmail.com>
2020-11-27 13:25:55 +0530

    Update CustomEntity2

--- src/main/java/lk/bit/web/entity/CustomEntity2.java
+++ src/main/java/lk/bit/web/entity/CustomEntity2.java
@@ -1,4 +1,16 @@
 package lk.bit.web.entity;
 
 public interface CustomEntity2 {
+
+    String getProductId();
+
+    String getProductName();
+
+    String getProductCategory();
+
+    String getProductSellingPrice();
+
+    String getProductBuyingPrice();
+
+    String getProductStatus();
 }
