a3476fec9e41ec8965fe972fccb71daba5660c88
LawrenceAsanka <asankalorance@gmail.com>
2020-12-04 12:23:51 +0530

    Create SupplierInvoiceDetailPK

---
+++ src/main/java/lk/bit/web/entity/SupplierInvoiceDetailPK.java
@@ -0,0 +1,18 @@
+package lk.bit.web.entity;
+
+import lombok.*;
+
+import javax.persistence.Embeddable;
+import java.io.Serializable;
+
+@Embeddable
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString
+public class SupplierInvoiceDetailPK implements Serializable {
+
+    private String invoiceNumber;
+    private String productId;
+}
