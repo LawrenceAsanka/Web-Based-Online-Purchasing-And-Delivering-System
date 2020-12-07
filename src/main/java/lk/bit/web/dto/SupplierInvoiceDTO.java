3c0cd0a90d7308c81c29d34e8bbe8dc03e0eba99
LawrenceAsanka <asankalorance@gmail.com>
2020-12-07 13:49:24 +0530

    Create SupplierInvoiceDTO

---
+++ src/main/java/lk/bit/web/dto/SupplierInvoiceDTO.java
@@ -0,0 +1,18 @@
+package lk.bit.web.dto;
+
+import lombok.*;
+
+import java.util.Date;
+import java.util.List;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString
+public class SupplierInvoiceDTO {
+
+    private String invoiceNumber;
+    private Date dateTime;
+    private List<SupplierInvoiceDetailDTO> invoiceDetail;
+}
