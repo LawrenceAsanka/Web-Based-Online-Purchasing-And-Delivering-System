cfc6bd969b078bbd503bdcf6c3dc0efc26ab07ff
LawrenceAsanka <asankalorance@gmail.com>
2020-12-07 17:00:07 +0530

    Create SupplierInvoiceDetailRepo

---
+++ src/main/java/lk/bit/web/repository/SupplierInvoiceDetailRepository.java
@@ -0,0 +1,8 @@
+package lk.bit.web.repository;
+
+import lk.bit.web.entity.SupplierInvoiceDetail;
+import lk.bit.web.entity.SupplierInvoiceDetailPK;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SupplierInvoiceDetailRepository extends JpaRepository<SupplierInvoiceDetail, SupplierInvoiceDetailPK> {
+}
