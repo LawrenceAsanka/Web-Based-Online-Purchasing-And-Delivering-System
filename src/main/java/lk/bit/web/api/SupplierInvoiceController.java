c86cc65d23a170ec2182e1b6b11a796717c5e8fe
LawrenceAsanka <asankalorance@gmail.com>
2020-12-07 16:58:52 +0530

    Create SupplierInvoiceRepo

--- src/main/java/lk/bit/web/api/SupplierInvoiceController.java
+++ src/main/java/lk/bit/web/api/SupplierInvoiceController.java
@@ -1,18 +1,27 @@
 package lk.bit.web.api;
 
+import lk.bit.web.business.custom.UserBO;
 import lk.bit.web.dto.SupplierInvoiceDTO;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 @CrossOrigin
 @RestController
 @RequestMapping("api/v1/supplierInvoices")
 public class SupplierInvoiceController {
 
+    @Autowired
+    private UserBO userBO;
+
     @ResponseStatus(HttpStatus.CREATED)
     @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
     private void saveSupplierInvoice(@RequestBody SupplierInvoiceDTO invoiceDetails){
+        if (!userBO.existUser(invoiceDetails.getUserId())) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
+        }
         System.out.println(invoiceDetails);
     }
 }
---
+++ src/main/java/lk/bit/web/repository/SupplierInvoiceRepository.java
@@ -0,0 +1,7 @@
+package lk.bit.web.repository;
+
+import lk.bit.web.entity.SupplierInvoice;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SupplierInvoiceRepository extends JpaRepository<SupplierInvoice, String> {
+}
