9079664936c61e3aaca078182e71d68e53d99007
LawrenceAsanka <asankalorance@gmail.com>
2020-12-10 10:48:16 +0530

    Create AdvertisementRespository

--- src/main/java/lk/bit/web/business/custom/impl/AdvertisementBOImpl.java
+++ src/main/java/lk/bit/web/business/custom/impl/AdvertisementBOImpl.java
@@ -4,8 +4,13 @@ import lk.bit.web.business.custom.AdvertisementBO;
 import org.springframework.stereotype.Component;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
+
 @Component
 public class AdvertisementBOImpl implements AdvertisementBO {
+
+    private static File file;
+    private String uploadDir;
 
     @Override
     public void saveAdvertisement(MultipartFile image, String advertisementName) {
---
+++ src/main/java/lk/bit/web/repository/AdvertisementRepository.java
@@ -0,0 +1,7 @@
+package lk.bit.web.repository;
+
+import lk.bit.web.entity.Advertisement;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AdvertisementRepository extends JpaRepository<Advertisement,Integer> {
+}
