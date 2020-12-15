06cbd0bec5c01d5f64e790aee0fe5dcd2c54ef43
LawrenceAsanka <asankalorance@gmail.com>
2020-12-15 11:10:32 +0530

    Create CustomerRepository

---
+++ src/main/java/lk/bit/web/repository/CustomerRepository.java
@@ -0,0 +1,7 @@
+package lk.bit.web.repository;
+
+import lk.bit.web.entity.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CustomerRepository extends JpaRepository<Customer, String> {
+}
