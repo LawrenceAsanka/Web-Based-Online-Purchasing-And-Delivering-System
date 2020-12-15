4d8f55f6c2e081ca175a4a0015af4e915503b3f0
LawrenceAsanka <asankalorance@gmail.com>
2020-12-15 11:11:41 +0530

    Create CustomerController

--- src/main/java/lk/bit/web/api/CustomerController.java
+++ src/main/java/lk/bit/web/api/CustomerController.java
@@ -1,5 +1,11 @@
 package lk.bit.web.api;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin
+@RestController
+@RequestMapping("api/v1/customers")
 public class CustomerController {
 }
