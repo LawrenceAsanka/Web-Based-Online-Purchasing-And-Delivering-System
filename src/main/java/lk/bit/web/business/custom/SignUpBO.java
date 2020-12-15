7f1783091a994d01b7a63badd84aad4f2ce00f96
LawrenceAsanka <asankalorance@gmail.com>
2020-12-15 12:29:12 +0530

    Create SignBO & SingBOImpl

---
+++ src/main/java/lk/bit/web/business/custom/SignUpBO.java
@@ -0,0 +1,8 @@
+package lk.bit.web.business.custom;
+
+import lk.bit.web.business.SuperBO;
+
+public interface SignUpBO extends SuperBO {
+
+    void saveCustomer();
+}
---
+++ src/main/java/lk/bit/web/business/custom/impl/SignUpBOImpl.java
@@ -0,0 +1,11 @@
+package lk.bit.web.business.custom.impl;
+
+import lk.bit.web.business.custom.SignUpBO;
+
+public class SignUpBOImpl implements SignUpBO{
+
+    @Override
+    public void saveCustomer() {
+
+    }
+}
