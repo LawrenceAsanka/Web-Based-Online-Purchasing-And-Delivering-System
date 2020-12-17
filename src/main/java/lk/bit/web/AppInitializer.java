28e4bf92490cc6a5d7458e8590a4644b48027ab9
LawrenceAsanka <asankalorance@gmail.com>
2020-12-17 09:00:31 +0530

    Add a saveCustomer method in CustomerController

--- src/main/java/lk/bit/web/AppInitializer.java
+++ src/main/java/lk/bit/web/AppInitializer.java
@@ -3,6 +3,7 @@ package lk.bit.web;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
--- src/main/java/lk/bit/web/api/CustomerController.java
+++ src/main/java/lk/bit/web/api/CustomerController.java
@@ -1,11 +1,28 @@
 package lk.bit.web.api;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import lk.bit.web.business.custom.CustomerBO;
+import lk.bit.web.dto.SignUpDTO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 @CrossOrigin
 @RestController
 @RequestMapping("api/v1/customers")
 public class CustomerController {
+
+    @Autowired
+    private CustomerBO customerBO;
+
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
+    private void saveCustomer(@RequestBody SignUpDTO signUpDetails){
+        if (signUpDetails.getFirstName().isEmpty() || signUpDetails.getEmail().isEmpty() ||
+        signUpDetails.getPassword().isEmpty()) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
+        }
+        customerBO.saveCustomer(signUpDetails);
+    }
 }
