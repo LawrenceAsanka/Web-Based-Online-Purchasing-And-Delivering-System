7ac2feb746883ab4ab2fa51da7bbe5620492d29d
LawrenceAsanka <asankalorance@gmail.com>
2020-12-22 08:20:24 +0530

    Autowired environment property

--- src/main/java/lk/bit/web/business/custom/impl/CustomerBOImpl.java
+++ src/main/java/lk/bit/web/business/custom/impl/CustomerBOImpl.java
@@ -6,6 +6,7 @@ import lk.bit.web.entity.Customer;
 import lk.bit.web.repository.CustomerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.Environment;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -21,8 +22,8 @@ public class CustomerBOImpl implements CustomerBO {
     private CustomerRepository customerRepository;
     @Autowired
     private PasswordEncoder passwordEncoder;
-    @Value("${spring.mail.username}")
-    private String getEmailAddress;
+    @Autowired
+    private Environment env;
 
     @Override
     public void saveCustomer(SignUpDTO signUpDetails) {
@@ -32,7 +33,7 @@ public class CustomerBOImpl implements CustomerBO {
 
         // send an email to the created customer
         SimpleMailMessage mail = new SimpleMailMessage();
-        mail.setFrom(getEmailAddress);
+        mail.setFrom(env.getRequiredProperty("spring.mail.username"));
         mail.setTo(signUpDetails.getEmail());
         mail.setSubject("Welcome to the VG Distributors e shopping");
         mail.setText("Thanks for registering with us!.Use your email address as your user name when logging.");
