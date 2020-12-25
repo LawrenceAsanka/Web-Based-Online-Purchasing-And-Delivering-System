421ffc83bc8ac2410beb11eca7933eb271ebe654
LawrenceAsanka <asankalorance@gmail.com>
2020-12-25 17:03:48 +0530

    Create ShopCategory Repo

--- src/main/java/lk/bit/web/entity/ShopCategory.java
+++ src/main/java/lk/bit/web/entity/ShopCategory.java
@@ -15,13 +15,13 @@ public class ShopCategory implements SuperEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "category_id",length = 20)
+    @Column(name = "category_id", length = 20)
     private int categoryId;
 
-    @Column(name = "category_name",length = 100,nullable = false)
+    @Column(name = "category_name", length = 50, nullable = false)
     private String categoryName;
 
-    @Column(length = 20,nullable = false)
-   private String status;
+    @Column(length = 20, nullable = false)
+    private String status;
 
 }
---
+++ src/main/java/lk/bit/web/repository/ShopCategory.java
@@ -0,0 +1,7 @@
+package lk.bit.web.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ShopCategory extends JpaRepository<Integer, lk.bit.web.entity.ShopCategory> {
+
+}
