b6d13dc83f4f6d0b1883050b6c6186d53c99223d
LawrenceAsanka <asankalorance@gmail.com>
2020-12-25 16:54:55 +0530

    Create ShopCategory Entity

---
+++ src/main/java/lk/bit/web/entity/ShopCategory.java
@@ -0,0 +1,27 @@
+package lk.bit.web.entity;
+
+import lombok.*;
+
+import javax.persistence.*;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Setter
+@Getter
+@ToString
+@Table(name = "shop_category")
+public class ShopCategory implements SuperEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "category_id",length = 20)
+    private int categoryId;
+
+    @Column(name = "category_name",length = 100,nullable = false)
+    private String categoryName;
+
+    @Column(length = 20,nullable = false)
+   private String status;
+
+}
