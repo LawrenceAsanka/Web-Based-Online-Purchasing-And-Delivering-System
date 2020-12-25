6230707a013f0a4d4e8c7778114613245de0fc48
LawrenceAsanka <asankalorance@gmail.com>
2020-12-25 17:56:45 +0530

    Update saveShopCategory()

--- src/main/java/lk/bit/web/api/ShopCategoryController.java
+++ src/main/java/lk/bit/web/api/ShopCategoryController.java
@@ -1,8 +1,10 @@
 package lk.bit.web.api;
 
 import lk.bit.web.business.custom.ShopCategoryBO;
+import lk.bit.web.dto.ShopCategoryDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
@@ -15,11 +17,11 @@ public class ShopCategoryController {
     private ShopCategoryBO shopCategoryBO;
 
     @ResponseStatus(HttpStatus.CREATED)
-    @PostMapping
-    private void saveShopCategory(String categoryName){
-        if (categoryName == null) {
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
+    private void saveShopCategory(@RequestBody ShopCategoryDTO shopCategoryDetails){
+        if (shopCategoryDetails.getCategoryName() == null) {
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
         }
-        shopCategoryBO.saveShopCategory(categoryName);
+        shopCategoryBO.saveShopCategory(shopCategoryDetails.getCategoryName());
     }
 }
