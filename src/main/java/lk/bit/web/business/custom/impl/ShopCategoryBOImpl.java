15793671fd096e51a882005b636b3b59687af057
LawrenceAsanka <asankalorance@gmail.com>
2020-12-25 17:15:42 +0530

    Create ShopCategoryBOImpl

---
+++ src/main/java/lk/bit/web/business/custom/impl/ShopCategoryBOImpl.java
@@ -0,0 +1,25 @@
+package lk.bit.web.business.custom.impl;
+
+import lk.bit.web.business.custom.ShopCategoryBO;
+
+public class ShopCategoryBOImpl implements ShopCategoryBO {
+    @Override
+    public void getAllShopCategories() {
+
+    }
+
+    @Override
+    public void getShopCategory(int categoryId) {
+
+    }
+
+    @Override
+    public void saveShopCategory(String categoryName) {
+
+    }
+
+    @Override
+    public void updateShopCategory(int categoryId) {
+
+    }
+}
