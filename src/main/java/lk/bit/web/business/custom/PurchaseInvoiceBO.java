e3ce51c323051fd23a1ac65512f192acbfaeba4e
LawrenceAsanka <asankalorance@gmail.com>
2021-03-31 14:08:07 +0530

    Create PurchaseInvoiceBO and Impl

---
+++ src/main/java/lk/bit/web/business/custom/PurchaseInvoiceBO.java
@@ -0,0 +1,6 @@
+package lk.bit.web.business.custom;
+
+import lk.bit.web.business.SuperBO;
+
+public interface PurchaseInvoiceBO extends SuperBO {
+}
--- src/main/java/lk/bit/web/repository/PurchaseInvoiceRepository.java
+++ src/main/java/lk/bit/web/repository/PurchaseInvoiceRepository.java
@@ -2,8 +2,12 @@ package lk.bit.web.repository;
 
 import lk.bit.web.entity.PurchaseInvoice;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface PurchaseInvoiceRepository extends JpaRepository<PurchaseInvoice, String> {
+
+    @Query(value = "SELECT id FROM purchase_invoice ORDER BY id DESC LIMIT 1", nativeQuery = true)
+    public String getLastPurchaseId();
 }
